baf5ec3c40da42e2fcb6b347cc17f0be843cf924
Sergey <mountaincer@mail.ru>
2025-10-14 09:07:55 +0700

    Add 5 main tasks + 2 additional tasks

---
+++ task_3.py
@@ -0,0 +1,20 @@
+world_champions = {
+    2002: 'Бразилия',
+    2006: 'Италия',
+    2010: 'Испания',
+    2014: 'Германия',
+    2018: 'Франция',
+}
+
+world_champions[2022] = 'Аргентина'
+for year, country in world_champions.items():
+    print(f'{year} - {country}')
+
+def is_country_champion(country):
+    if country in world_champions.values():
+        print(f'{country} cтановилась чемпионом мира по футболу в 21 веке!')
+    else:
+        print(f'{country} не выигрывала чемпионат мира по футболу в 21 веке.')
+
+test_country = 'Италия'
+is_country_champion(test_country)
